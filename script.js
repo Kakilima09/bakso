60c9909ba6c5e7c5f1a04613f31b3f6c94d856c6
Kakilima09 <153986755+Kakilima09@users.noreply.github.com>
2025-07-26 10:49:36 +0700

    Add files via upload
    perubahan humberger menu

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 // Mobile Menu Toggle
-document.querySelector('.mobile-menu').addEventListener('click', function() {
-    document.querySelector('nav ul').classList.toggle('show');
+const mobileMenuBtn = document.getElementById('mobile-menu-btn');
+const navMenu = document.getElementById('nav-menu');
+
+mobileMenuBtn.addEventListener('click', function() {
+    navMenu.classList.toggle('show');
+    
+    // Ganti ikon antara hamburger dan close
+    const icon = this.querySelector('i');
+    if (navMenu.classList.contains('show')) {
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-times');
+    } else {
+        icon.classList.remove('fa-times');
+        icon.classList.add('fa-bars');
+    }
+});
+
+// Tutup menu mobile ketika mengklik link
+const navLinks = document.querySelectorAll('#nav-menu a');
+navLinks.forEach(link => {
+    link.addEventListener('click', function() {
+        navMenu.classList.remove('show');
+        mobileMenuBtn.querySelector('i').classList.remove('fa-times');
+        mobileMenuBtn.querySelector('i').classList.add('fa-bars');
+    });
 });
 
 // Smooth Scrolling
